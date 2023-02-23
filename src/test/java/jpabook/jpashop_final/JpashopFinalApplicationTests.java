b69cc59e19efaebb3a53003075cdd611cc78d701
seo <syh05247@naver.com>
2023-02-23 22:48:22 +0900

    Feat: 프로젝트 설정
    Spring-boot 기본 설정

---
+++ src/test/java/jpabook/jpashop_final/JpashopFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package jpabook.jpashop_final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpashopFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
