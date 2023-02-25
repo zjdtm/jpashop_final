7f9b4098114647a2619f143dd287499279d2d7fc
seo <syh05247@naver.com>
2023-02-25 22:36:56 +0900

    Feat: 도메인 추가
    Member, Order, Delivery, DeliveryStatus, OrderItem, Item, Category, Book, Album, Movie, Address 도메인 추가

---
+++ src/main/java/jpabook/jpashop_final/domain/Delivery.java
@@ -0,0 +1,29 @@
+package jpabook.jpashop_final.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import static jakarta.persistence.FetchType.*;
+
+@Entity
+@Getter
+@Setter
+public class Delivery {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "delivery_id")
+    private Long id;
+
+    @OneToOne(mappedBy = "delivery", fetch = LAZY)
+    private Order order;
+
+    @Embedded
+    private Address address;
+
+    @Enumerated(EnumType.STRING)
+    private DeliveryStatus stats;
+
+
+}
---
+++ src/main/java/jpabook/jpashop_final/domain/DeliveryStatus.java
@@ -0,0 +1,5 @@
+package jpabook.jpashop_final.domain;
+
+public enum DeliveryStatus {
+    READY, COMP
+}
